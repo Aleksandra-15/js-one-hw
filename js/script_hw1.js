525deee475d674494de4792edc01c8f4a4ddd9d8
Aleksandra-15~ <saha02247@gmail.com01~>
2024-02-01 22:05:10 +0200

    file js

---
+++ js/script_hw1.js
@@ -0,0 +1,49 @@
+/* const age = 15;
+console.log(15) ; 
+
+const name = 'Aleksandra';
+console.log();
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = 'Life is a amazing';
+console.log(myString);
+
+'use strict';
+const str = 'Open Sans';
+const meNumber = '0984262270';
+const prise = 10;
+const total = prise + meNumber;
+
+console.log(total);
+console.log(str.lenght);
+
+console.log(str.toUpperCase());
+
+
+const myNull = null;
+console.log(null);
+
+ */
+
+
+
+
+/* const result = prompt('Як тебе звати?');
+console.log(result);  
+
+const answer = confirm();
+if (answer) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію не підтверджено");
+} 
+
+
+const answer = confirm('Ця дія небезпечна!');
+if (answer){
+  alert('Дякую за підтвердження!')
+} else{
+  alert ('Дію відмінено')
+} */
