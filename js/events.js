52c2ffd466f46aafbd351ed8f9719f51a36f8099
Aleksandra-15~ <saha02247@gmail.com01~>
2024-02-28 18:49:47 +0200

    file

--- js/events.js
+++ js/events.js
@@ -1,14 +1,19 @@
-"use strict";
-const openModalBtn = document.querySelector('button[data-action="open-modal"]');
-const closeModalBtn = document.querySelector(
-  'button[data-action="close-modal"]'
-);
-const modal = document.querySelector(". js-backdrop");
+const openModalBtn = document.querySelector('[data-action="open-modal"]');
+const closeModalBtn = document.querySelector('[data-action="close-modal"]');
+const modal = document.querySelector(".modal");
+const backdrop = document.querySelector(".js-backdrop");
 
-openModalBtn.addEventListener("click", () => {
+function toggleModal () {
+  document.body.classList.toggle("show-modal");
+}
+
+openModalBtn.addEventListener("click", toggleModal);
+closeModalBtn.addEventListener("click", toggleModal);
+
+/* openModalBtn.addEventListener("click", () => {
   modal.classList.add("show");
 });
 
 closeModalBtn.addEventListener("click", () => {
   modal.classList.remove("show");
-});
+}); */
